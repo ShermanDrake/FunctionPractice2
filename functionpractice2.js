b362175347ee9bfa74b44d03a25574707bb5deff
ShermanDrake <shermandrake@comcast.net>
2015-09-15 17:46:23 -0600

    second commit

---
+++ functionpractice2.js
@@ -0,0 +1,97 @@
+// var getValue = function(object, property) {
+
+
+// 	return object[property]
+// 	}
+
+
+// var value = getValue({name: 'Luisa', age: 25, city: 'Boulder'}, 'name') 
+
+// console.log(value)
+
+
+// var getName = function(object) {
+
+// 	return object.name
+// }
+// var name = getName({name: 'Luisa', age: 25})
+
+// console.log(name)
+
+
+
+// var totalLetters = function(lettersArray) {
+
+// 	var count = 0
+
+// 	lettersArray.forEach(function(word){
+// 		count += word.length 
+
+// 	})
+
+	
+// 		return count
+// }
+
+// console.log(totalLetters(['javascript', 'is', 'awesome']))
+// console.log(totalLetters(['what', 'happened', 'to', 'my', 'function']))
+
+
+
+// var keyValue = function(arg1, arg2) {
+
+// 	var object = {}
+
+// 	object[arg1] = arg2
+
+// 	return object 
+
+// }
+// var value = keyValue('city', 'Denver')
+
+
+// console.log(value)
+
+// var cities = {city: 'san francisco', population: 85000}
+
+
+
+var negativeIndex = function(indexArray) {
+
+	var count = 0
+
+	indexArray.forEach(function(string){
+		count += word.length 
+
+	})
+
+	
+		return count
+}
+
+console.log(negativeIndex(['a', 'b', 'c', 'd', 'e'], -2))
+console.log(negativeIndex(['jerry', 'sarah', 'sally'], -1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
